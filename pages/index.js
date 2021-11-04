8577b896b4f6564413d83c05e4dfcfd52f727b1d
Matt <mqunell@gmail.com>
2021-11-03 17:58:15 -0700

    5. Dynamic Routes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import Date from '../components/date';
 import Layout, { siteTitle } from '../components/layout';
 import { getSortedPostsData } from '../lib/posts';
 import utilStyles from '../styles/utils.module.css';
@@ -31,10 +32,6 @@ export default function Home({ allPostsData }) {
 					Hey, I'm Matt. I have some experience with standard React via Create
 					React App, and now I'm learning Next.js!
 				</p>
-
-				<Link href="posts/first-post">
-					<a>First Post</a>
-				</Link>
 			</section>
 
 			<section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
@@ -42,11 +39,13 @@ export default function Home({ allPostsData }) {
 				<ul className={utilStyles.list}>
 					{allPostsData.map(({ id, date, title }) => (
 						<li className={utilStyles.listItem} key={id}>
-							{title}
+							<Link href={`/posts/${id}`}>
+								<a>{title}</a>
+							</Link>
 							<br />
-							{id}
-							<br />
-							{date}
+							<small className={utilStyles.lightText}>
+								<Date dateString={date} />
+							</small>
 						</li>
 					))}
 				</ul>
---
+++ pages/posts/[id].js
@@ -0,0 +1,49 @@
+import Head from 'next/head';
+import Date from '../../components/date';
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import utilStyles from '../../styles/utils.module.css';
+
+/**
+ * SSG: Pre-render all the specified paths
+ *
+ * "fallback: false" means that any paths not returned by getStaticPaths()
+ * will result in a 404 page
+ */
+export async function getStaticPaths() {
+	const paths = getAllPostIds();
+
+	return {
+		paths,
+		fallback: false,
+	};
+}
+
+// SSG: Get props for component
+export async function getStaticProps({ params }) {
+	const postData = await getPostData(params.id);
+
+	return {
+		props: {
+			postData,
+		},
+	};
+}
+
+export default function Post({ postData }) {
+	return (
+		<Layout>
+			<Head>
+				<title>{postData.title}</title>
+			</Head>
+
+			<article>
+				<h1 className={utilStyles.headingXl}>{postData.title}</h1>
+				<div className={utilStyles.lightText}>
+					<Date dateString={postData.date} />
+				</div>
+				<div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+			</article>
+		</Layout>
+	);
+}
